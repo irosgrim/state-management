a0c107778fbf7bfc4c1e5922c439fbf8e247b9b1
Ioan Rosgrim <ioan.rosgrim@ingka.ikea.com>
2023-09-19 13:19:21 +0200

    path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/state-management",
+  base: "/state-management/",
   plugins: [react()],
 })
